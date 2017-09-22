166d8805dbb2009067654553bd521defe094b605
Max Battcher <maxwell.battcher@stantec.com>
2017-09-22 18:17:16 -0400

    Add Typescript return types

--- snabbdom-jsx.d.ts
+++ snabbdom-jsx.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="./jsx.d.ts" />
-declare export function html(tag: any, attrs: any, children: any)
-declare export function svg(tag: any, attrs: any, children: any)
-declare export function JSX(nsURL: string, defNS?: any, modules?: any)
+import { VNode } from 'snabbdom/vnode'
+
+export function html(tag: any, attrs: any, children: any): VNode
+export function svg(tag: any, attrs: any, children: any): VNode
+export function JSX(nsURL: string, defNS?: any, modules?: any): VNode
